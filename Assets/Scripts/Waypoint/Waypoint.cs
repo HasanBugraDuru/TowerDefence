7ce017f689eb59733dd481b0072629a07c1454e0
Hasan Buğra Duru <hasanbugraduru@hotmail.com>
2024-05-04 23:06:06 +0300

    Waypoint And Handle

---
+++ Assets/Scripts/Waypoint/Waypoint.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField] private Vector3[] points;
+    public Vector3[] Points => points;
+    public Vector3 CurrentPosition => _currentPosition;
+
+    private Vector3 _currentPosition;
+    private bool _gameStarted = false;
+
+    private void Start()
+    {
+        _gameStarted = true;
+        _currentPosition = transform.position;    
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!_gameStarted && transform.hasChanged)
+        {
+            _currentPosition = transform.position;
+        }
+        for (int i = 0; i < points.Length; i++) 
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(points[i] + _currentPosition, 0.5f);
+
+            if(i < points.Length - 1) 
+            {
+                Gizmos.color = Color.gray;
+                Gizmos.DrawLine(points[i] + _currentPosition, points[i + 1] + _currentPosition);  
+            }
+        }
+    }
+}
